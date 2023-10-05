457c0a98e656acbf978f27c097de46467cbca705
Commodoreprime <35617951+Commodoreprime@users.noreply.github.com>
2023-10-05 02:03:35 -0800

    Modified REH updating script to be more robust (also broke out flatpak arguments into their own variable)

--- share/remote.oss.update.vscodium-reh.sh
+++ share/remote.oss.update.vscodium-reh.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
+VSCODE_VERSION="$1"
+VSCODE_COMMIT_ID="$2"
+
 set -uexo pipefail
 
 VSCODIUM_DIR="${HOME}/.vscodium-server"
-RELEASE_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' 'https://github.com/VSCodium/vscodium/releases/latest')"
-RELEASE="${RELEASE_URL##*/}"
-PACKAGE="vscodium-reh-linux-x64-${RELEASE}.tar.gz"
-DOWNLOAD_URL="https://github.com/VSCodium/vscodium/releases/download/${RELEASE}/${PACKAGE}"
 
 mkdir -p "${VSCODIUM_DIR}"
 pushd "${VSCODIUM_DIR}"
 
-curl -Ls -o "${VSCODIUM_DIR}/${PACKAGE}" "${DOWNLOAD_URL}"
-COMMIT_ID="$(tar -xf "${VSCODIUM_DIR}/${PACKAGE}" ./product.json -O | grep -m1 'commit' | cut -d'"' -f4)"
+# Because VSCodium builds have sub version numbers (grrr...) we need to get a list of tags via GitHub API
+#  then figure it out based on $VSCODE_VERSION and $VSCODE_COMMIT_ID. We only keep the version that matches commit ID.
+for release_tag in $(curl -s 'https://api.github.com/repos/VSCodium/vscodium/tags' | grep '"name"' | grep "${VSCODE_VERSION}" | cut -d'"' -f4); do
+    PACKAGE="vscodium-reh-linux-x64-${release_tag}.tar.gz"
+    DOWNLOAD_URL="https://github.com/VSCodium/vscodium/releases/download/${release_tag}/${PACKAGE}"
+
+    ARCHIVE_PATH="${VSCODIUM_DIR}/${PACKAGE}"
+
+    curl -Ls -o "${ARCHIVE_PATH}" "${DOWNLOAD_URL}"
+    COMMIT_ID="$(tar -xf "${ARCHIVE_PATH}" ./product.json -O | grep -m1 'commit' | cut -d'"' -f4)"
+    set +e
+    if [ "${VSCODE_COMMIT_ID}" != "${COMMIT_ID}" ]; then
+        rm "${ARCHIVE_PATH}"
+        continue
+    fi
+    set -e
+    break
+done
 
 BIN_DIR="${VSCODIUM_DIR}/bin/${COMMIT_ID}"
 mkdir -p "${BIN_DIR}"
 pushd "${BIN_DIR}"
-tar -xf "${VSCODIUM_DIR}/${PACKAGE}"
+tar -xf "${ARCHIVE_PATH}"
 popd
 
 ln -sfT "${BIN_DIR}" "${VSCODIUM_DIR}/bin/current"
-rm "${VSCODIUM_DIR}/${PACKAGE}"
+rm "${ARCHIVE_PATH}"
